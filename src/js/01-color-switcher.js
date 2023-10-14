a1e7f416f73c276cbeec4e0ecd7e0717176ea943
Vladyslav Sahaidak <svg.231@gmail.com>
2023-10-14 12:04:05 +0200

    'weDoneWithExersisTwo'

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,8 @@
 
 // console.log('jeh hedder en pindsvin')
-
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 const btnStart = document.querySelector('[data-start]');
 const btnStop = document.querySelector('[data-stop]');
 const bodyGetToWork = () => document.querySelector('body').style.backgroundColor = `${getRandomHexColor()}`
@@ -13,7 +15,5 @@ btnStop.addEventListener('click', () => {
     clearInterval(timeUpdateColor);
     btnStart.style.pointerEvents = 'auto';
 })
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-}
+
 
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,7 +3,6 @@ import Notiflix from 'notiflix';
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import { Report } from 'notiflix/build/notiflix-report-aio';
-
 const date = new Date();
 const inputEl = document.querySelector('#datetime-picker');
 const btnStart = document.querySelector('[data-start]')
@@ -12,68 +11,49 @@ const inputHours = document.querySelector('[data-hours]')
 const inputMinutes = document.querySelector('[data-minutes]')
 const inputSeconds = document.querySelector('[data-seconds]')
 
-console.log(inputDays)
-console.log(inputHours)
-console.log(inputMinutes)
-console.log(inputSeconds)
+
+let timeUpdateTime = null;
+
+btnStart.style.pointerEvents = 'none';
+btnStart.style.opacity = 0.5;
 
 
-Notiflix.Report.init({
-    position: 'center-bottom',
-    className: 'notiflix-report',
-    width: '320px',
-    backgroundColor: '#f8f8f8',
-    borderRadius: '50px',
-});
-const options = {
-    enableTime: true,
-    enableSeconds: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    theme: "dark",
-    // minDate: "today",
-    maxDate: new Date().fp_incr(22),
-    // disable: [
-    //     function (date) {
-    //         return (date.getDay() === 0 || date.getDay() === 6);
-    //     }
-    // ],
-    locale: {
-        "firstDayOfWeek": 1 // start week on Monday
-    },
-    onClose(selectedDates) {
-        console.log(selectedDates[0]);
-        if (selectedDates[0] < date) {
-            Notiflix.Report.failure('ERROR', 'Please choose a date in the future', 'Close');
+
+
+function padStart() {
+    if (+inputSeconds.textContent >= 0) {
+        inputSeconds.textContent = +inputSeconds.textContent - 1;
+
+    }
+    if (+inputSeconds.textContent === -1) {
+        inputSeconds.textContent = 60;
+    }
+
+    if (+inputSeconds.textContent === 0) {
+        inputMinutes.textContent = +inputMinutes.textContent - 1;
+
+    }
+    if (+inputMinutes.textContent === -1) {
+        inputHours.textContent -= 1;
+        inputMinutes.textContent = 60;
+    }
+
+    if (+inputHours.textContent === 0) {
+        inputHours.textContent = +inputHours.textContent - 1;
+        if (+inputHours.textContent === -1) {
+            inputDays.textContent -= 1;
+            inputHours.textContent = 24;
+            if (+inputDays.textContent === -1) {
+                inputSeconds.textContent = 0;
+                inputMinutes.textContent = 0;
+                inputHours.textContent = 0;
+                inputDays.textContent = 0;
+                clearInterval(timeUpdateTime);
+            }
         }
-        const objectDate = convertMs(selectedDates[0])
-        console.log(objectDate)
-        console.log(parseFloat((objectDate.days / 365).toFixed(1)));
-        inputDays.textContent = `${parseFloat((objectDate.days).toFixed(1)) - 19643}`
-        inputHours.textContent = `${parseFloat((objectDate.hours).toFixed(1))}`
-        inputMinutes.textContent = `${parseFloat((objectDate.minutes).toFixed(1))}`
-        inputSeconds.textContent = `${parseFloat((objectDate.seconds).toFixed(1))}`
-    },
-};
-
-flatpickr(inputEl, options);
-
-
-
-function padStart({ days, hours, minutes, seconds }) {
-    setInterval(function ({ days, hours, minutes, seconds }) {
-        console.log("getDay(): ", date.getDay());
-        // Возвращает год из 4 цифр
-        console.log("getFullYear(): ", date.getFullYear());
-        // Возвращает час
-        console.log("getHours(): ", date.getHours());
-        // Возвращает минуты
-        console.log("getMinutes(): ", date.getMinutes());
-        // Возвращает секунды
-        console.log("getSeconds(): ", date.getSeconds());
-    }, 1000);
+    }
 }
+
 function convertMs(ms) {
     // Number of milliseconds per unit of time
     const second = 1000;
@@ -93,4 +73,65 @@ function convertMs(ms) {
     return { days, hours, minutes, seconds };
 }
 
+
+
+
+Notiflix.Report.init({
+    width: '280px',
+    position: 'right-top', // не можу ніяк зрозумічит чому воно не працює
+    distance: '10px',
+    borderRadius: '50px',
+    backOverlayColor: 'rgba(1,2,3,4.9)',
+    svgSize: '80px',
+    backgroundColor: '#83f1ea',
+});
+const options = {
+    enableTime: true,
+    enableSeconds: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    theme: "dark", // не можу ніяк зрозумічит чому воно не працює
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d",
+    maxDate: new Date().fp_incr(22),
+    disable: [
+        function (date) {
+            return (date.getDay() === 0 || date.getDay() === 6);
+        }
+    ],
+    locale: {
+        "firstDayOfWeek": 1
+    },
+    onClose(selectedDates) {
+        console.log(selectedDates[0]);
+        if (selectedDates[0] < date) {
+
+            Notiflix.Report.failure('ERROR', 'Please choose a date in the future', 'Close');
+        }
+        const objectDate = convertMs(selectedDates[0])
+        clearInterval(timeUpdateTime);
+        inputDays.textContent = `${parseFloat((objectDate.days).toFixed(1)) - 19643}`
+        inputHours.textContent = `${parseFloat((objectDate.hours).toFixed(1))}`
+        inputMinutes.textContent = `${parseFloat((objectDate.minutes).toFixed(1))}`
+        inputSeconds.textContent = `${parseFloat((objectDate.seconds).toFixed(1))}`
+        if (+inputDays.textContent >= 1) {
+            btnStart.style.pointerEvents = 'auto';
+            btnStart.style.opacity = 1;
+        }
+        if (+inputDays.textContent <= 1) {
+            btnStart.style.pointerEvents = 'none';
+            btnStart.style.opacity = 0.5;
+        }
+    },
+};
+flatpickr(inputEl, options);
+
+
+btnStart.addEventListener('click', () => {
+    timeUpdateTime = setInterval(padStart, 1000);
+    btnStart.style.pointerEvents = 'none';
+    btnStart.style.opacity = 0.5;
+})
 
