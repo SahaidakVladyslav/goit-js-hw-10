d9f51f9e93bfb4950a7c8ecf3f70e7c9b231d759
Vladyslav Sahaidak <svg.231@gmail.com>
2023-10-25 19:55:45 +0200

    fix bugs with timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -10,48 +10,64 @@ const inputDays = document.querySelector('[data-days]')
 const inputHours = document.querySelector('[data-hours]')
 const inputMinutes = document.querySelector('[data-minutes]')
 const inputSeconds = document.querySelector('[data-seconds]')
-
-const todayDate = new Date()
-
 let timeUpdateTime = null;
 
-btnStart.style.pointerEvents = 'none';
-btnStart.style.opacity = 0.5;
-
-
-
-
-function padStart() {
+const todayDate = new Date()
+console.log()
+const btnNone = () => {
+    btnStart.style.opacity = 0.5;
+    btnStart.style.pointerEvents = 'none';
+}
+const btnAuto = () => {
+    btnStart.style.pointerEvents = 'auto';
+    btnStart.style.opacity = 1;
+}
+const inputTextContentValue = (dateFormat) => {
+    inputDays.textContent = `${parseFloat((dateFormat.days).toFixed(1)) - 19655}`
+    inputHours.textContent = `${parseFloat((dateFormat.hours).toFixed(1)) - 17}`
+    inputMinutes.textContent = `${parseFloat((dateFormat.minutes).toFixed(1)) - todayDate.getMinutes()}`
+    inputSeconds.textContent = `${parseFloat((dateFormat.seconds).toFixed(1))}`
+}
+const emptyTextContent = () => {
+    inputDays.textContent = `0`
+    inputHours.textContent = `0`
+    inputMinutes.textContent = `0`
+    inputSeconds.textContent = `0`
+}
+function ifShuldTimerStart() {
     if (+inputSeconds.textContent >= 0) {
-        inputSeconds.textContent = +inputSeconds.textContent - 1;
-
-    }
-    if (+inputSeconds.textContent === -1) {
-        inputSeconds.textContent = 60;
-    }
-
-    if (+inputSeconds.textContent === 0) {
-        inputMinutes.textContent = +inputMinutes.textContent - 1;
-
-    }
-    if (+inputMinutes.textContent === -1) {
-        inputHours.textContent -= 1;
-        inputMinutes.textContent = 60;
-    }
-
-    if (+inputHours.textContent === 0) {
-        inputHours.textContent = +inputHours.textContent - 1;
-        if (+inputHours.textContent === -1) {
-            inputDays.textContent -= 1;
-            inputHours.textContent = 24;
-            if (+inputDays.textContent === -1) {
-                inputSeconds.textContent = 0;
-                inputMinutes.textContent = 0;
-                inputHours.textContent = 0;
-                inputDays.textContent = 0;
-                clearInterval(timeUpdateTime);
+        if (+inputMinutes.textContent >= 0) {
+            if (+inputHours.textContent >= 0) {
+                if (+inputDays.textContent >= 0) {
+                    return btnAuto()
+                }
             }
         }
+    }
+
+
+}
+function padStart() {
+    let totalSeconds = +inputDays.textContent * 24 * 60 * 60 +
+        +inputHours.textContent * 60 * 60 +
+        +inputMinutes.textContent * 60 +
+        +inputSeconds.textContent;
+
+    totalSeconds -= 1;
+
+    if (totalSeconds < 0) {
+        emptyTextContent();
+        clearInterval(timeUpdateTime);
+    } else {
+        const days = Math.floor(totalSeconds / (24 * 60 * 60));
+        const hours = Math.floor((totalSeconds % (24 * 60 * 60)) / (60 * 60));
+        const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
+        const seconds = totalSeconds % 60;
+
+        inputDays.textContent = days;
+        inputHours.textContent = hours;
+        inputMinutes.textContent = minutes;
+        inputSeconds.textContent = seconds;
     }
 }
 
@@ -74,6 +90,7 @@ function convertMs(ms) {
     return { days, hours, minutes, seconds };
 }
 
+btnNone()
 
 
 
@@ -108,21 +125,15 @@ const options = {
     onClose(selectedDates) {
         console.log(selectedDates[0]);
         clearInterval(timeUpdateTime);
-
         if (selectedDates[0].getTime() <= todayDate.getTime()) {
             Notiflix.Report.failure('ERROR', 'Please choose a date in the future', 'Close');
-            btnStart.style.opacity = 0.5;
-            return btnStart.style.pointerEvents = 'none';
+            emptyTextContent()
+            return btnNone()
         }
+
         const objectDate = convertMs(selectedDates[0])
-        inputDays.textContent = `${parseFloat((objectDate.days).toFixed(1)) - 19653}`
-        inputHours.textContent = `${parseFloat((objectDate.hours).toFixed(1))}`
-        inputMinutes.textContent = `${parseFloat((objectDate.minutes).toFixed(1))}`
-        inputSeconds.textContent = `${parseFloat((objectDate.seconds).toFixed(1))}`
-        if (+inputDays.textContent >= 1) {
-            btnStart.style.pointerEvents = 'auto';
-            btnStart.style.opacity = 1;
-        }
+        inputTextContentValue(objectDate)
+        ifShuldTimerStart()
 
     },
 };
@@ -131,7 +142,6 @@ flatpickr(inputEl, options);
 
 btnStart.addEventListener('click', () => {
     timeUpdateTime = setInterval(padStart, 1000);
-    btnStart.style.pointerEvents = 'none';
-    btnStart.style.opacity = 0.5;
+    btnNone()
 })
 
